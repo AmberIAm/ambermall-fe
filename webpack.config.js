319896d9a2b14fff81e0769e2e2060f94ded7e33
Amber <AmberIAm@yeah.net>
2017-08-26 11:15:39 +0800

    initial

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var webpack           = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path              = require('path');
+// 获取html-webpack-plugin参数的方法
 var getHtmlConfig = function(name){
 	return {
 		template: './src/view/' + name + '.html',
@@ -11,6 +12,7 @@ var getHtmlConfig = function(name){
  		chunks: ['common', name]
 	}
 }
+// webpack配置
 var config = {
      entry: {
      	'common': ['./src/page/common/index.js', 'webpack-dev-server/client?http://localhost:8088/'],
@@ -22,27 +24,33 @@ var config = {
          publicPath : '/dist',
          filename: 'js/[name].js'
      },
+     // 引进jquery
      externals: {
      	'jquery': 'window.jQuery'
      },
      module: {
      	loaders: [
      		{
+     			// 解析css
      			test: /\.css$/, 
      			loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader' })
      		},
      		{
+     			// 解析图片和icon-font
      			test: /\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, 
      			loader: 'url-loader?limit=100&name=resource/[name].[ext]'
      		}
      	]
      },
      plugins: [
+     	// 独立通用模块至base.js
      	new webpack.optimize.CommonsChunkPlugin({
      		name    : 'common',
      		filename: 'js/base.js'
      	}),
+     	// css单独打包
      	new ExtractTextPlugin('css/[name].css'),
+     	// html模板的处理
      	new HtmlWebpackPlugin(getHtmlConfig('index')),
      	new HtmlWebpackPlugin(getHtmlConfig('login')),
      ]
