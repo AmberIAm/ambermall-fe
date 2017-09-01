1f93733b2a324bccd4b2f7feb7c1916837f0bad1
Amber <AmberIAm@yeah.net>
2017-09-01 23:48:05 +0800

    dev-cart

--- src/service/cart-service.js
+++ src/service/cart-service.js
@@ -29,6 +29,72 @@ var _cart = {
             success: resolve,
             error: reject
         });
+    },
+	// 获取购物车列表
+    getCartList: function (resolve, reject) {
+        _common.request({
+            url: _common.getServerUrl('/cart/list.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    // 选择购物车商品
+    selectProduct: function(productId, resolve, reject){
+        _common.request({
+            url: _common.getServerUrl('/cart/select.do'),
+            data: {
+                productId: productId
+            },
+            success: resolve,
+            error: reject
+        });
+    },
+    // 取消选择购物车商品
+    unselectProduct: function(productId, resolve, reject){
+        _common.request({
+            url: _common.getServerUrl('/cart/un_select.do'),
+            data: {
+                productId: productId
+            },
+            success: resolve,
+            error: reject
+        });
+    },
+    // 选择全部购物车商品
+    selectAllProduct: function(resolve, reject){
+        _common.request({
+            url: _common.getServerUrl('/cart/select_all.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    // 取消选中全部购物车商品
+    unselectAllProduct: function(resolve, reject){
+        _common.request({
+            url: _common.getServerUrl('/cart/un_select_all.do'),
+            success: resolve,
+            error: reject
+        });
+    },
+    // 更新购物车商品数量
+    updateProduct: function(productInfo, resolve, reject){
+        _common.request({
+            url: _common.getServerUrl('/cart/update.do'),
+            data: productInfo,
+            success: resolve,
+            error: reject
+        });
+    },
+    // 删除指定商品
+    deleteProduct: function(productIds, resolve, reject){
+        _common.request({
+            url: _common.getServerUrl('/cart/delete_product.do'),
+            data: {
+                productIds: productIds
+            },
+            success: resolve,
+            error: reject
+        });
     }
 };
 module.exports = _cart;
